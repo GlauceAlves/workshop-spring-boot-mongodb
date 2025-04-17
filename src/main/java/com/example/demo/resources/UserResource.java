b1e96e0857bcdc5971c26a6d754a41cfe16e40f8
Glauce Alves <glaucemail@gmail.com>
2025-04-17 15:59:55 -0300

    Deletando usuário por id.

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,16 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 		
 	}
+	
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<UserDTO> delete(@PathVariable String id) {
+		service.delete(id);
+ 		return ResponseEntity.noContent().build();
+		
+	}
+	
+	
 	
 //   @RequestMapping(method=RequestMethod.GET)
 //   		User maria = new User("1","Maria Brown","maria@gmail.com");
